07a11c58d1ea2e84c93b2c2c10d31056d77523fe
kolomyya <andriyzn4@gmail.com>
2019-04-25 10:22:05 -0500

    fixed

--- google-storage-bucket.tf
+++ google-storage-bucket.tf
@@ -1,13 +1,13 @@
 resource "google_storage_bucket" "bucket" {
-name = "${var.bucket_name}"
-location = "${var.region}"
-storage_class = "REGIONAL"
+  name = "${var.bucket_name}"
+  location = "${var.region}"
+  storage_class = "REGIONAL"
+
+  tags  {
+    source      = "../"
+    name        = "${var.app_name}"
+    namespace   = "${var.namespace}"
+    environment = "${var.environment}"
+   }
 }
 
-
-module "label" {
-  source      = "../"
-  name        = "${var.app_name}"
-  namespace   = "${var.namespace}"
-  environment = "${var.environment}"
-}
