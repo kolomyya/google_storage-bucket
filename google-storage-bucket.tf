7920ac4b7baa6c748b2758856c9b59ae198bed9a
kolomyya <andriyzn4@gmail.com>
2019-04-27 22:42:14 -0500

    added defaults variables

--- google-storage-bucket.tf
+++ google-storage-bucket.tf
@@ -2,15 +2,13 @@ resource "google_storage_bucket" "bucket" {
   name = "${var.bucket_name}"
   location = "${var.region}"
   storage_class = "REGIONAL"
- 
-  #  source      = "../"
-  #  name        = "${var.app_name}"
-  #  namespace   = "${var.namespace}"
-  #  environment = "${var.environment}"
-  # }
+
+  project = "fuchicopr"
+}
 
 
-#resource "google_storage_bucket_acl" "image-store-acl" {
-#  bucket = "${google_storage_bucket.#{var.bucket_name}.name}"
-#  predefined_acl = "publicreadwrite"
-#}
+resource "google_storage_bucket_object" "content_folder" {
+  name          = "empty_directory/"
+  content       = "Not really a directory, but it's empty."
+  bucket        = "${google_storage_bucket.storage_bucket.name}"
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,35 @@
-variable "bucket_name" {}
-variable "app_name"  {}
-variable "region" {}
-variable "namespace"  {}
-variable "environment"  {}
-variable "cpath"  {}  // terraform service acount
+variable "credentials" {
+  default = "acount.json"
+}
+
+variable "region" {
+  default = "us-central1" # Iowa
+}
+
+variable "bucket_name" {
+  default = "my-bucket"
+}  
+
+variable "location" {
+  default = "US"
+}
+
+variable "storage-class" {
+  default = "REGIONAL"
+}
+
+
+
+
+
+
+
+
+
+
+#variable "bucket_name" {}
+#variable "app_name"  {}
+#variable "region" {}
+#variable "namespace"  {}
+#variable "environment"  {}
+#variable "cpath"  {}  // terraform service acount
