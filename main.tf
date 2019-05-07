6301ce4890c658bd177123974b418645bc70e634
kolomyya <andriyzn4@gmail.com>
2019-05-06 23:34:08 -0500

    added triggers for folders

--- main.tf
+++ main.tf
@@ -5,26 +5,36 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "COLDLINE" {
-
   name   = "fuchicorp_bucket"
   storage_class = "COLDLINE" 
   location = "us-central1"
 }
 
 resource "google_storage_bucket_object" "folder" {
-  name = "test"
-  content = "Hello World"
-  bucket  = "${google_storage_bucket.COLDLINE.name}"
+    bucket  = "${google_storage_bucket.COLDLINE.name}"
+    count = "${length(var.folders)}"
+
+    triggers {
+      folders  =  "${element(var.folders, count.index)}"
+    }
 }
 
 
-#terraform {
-#  backend "gcs" {
-#    bucket  = "fuchicorp_bucket"
-#    prefix    = "test/terraform.tfstate"    #A path to the data you want to upload
-#   project = "focus-surfer-237100"
+
+
+
+
+
+
+
+
+
+
+
+#resource "google_storage_bucket_acl" "fuchicorp_bucket-acl" {
+#  bucket = "${google_storage_bucket.fuchicorp_bucket.name}"
 #  }
-#}
+
 
 #role_entity = [
 #    "${compact(var.role_entity)}",
@@ -35,4 +45,13 @@ resource "google_storage_bucket_object" "folder" {
 #  description = "List of role/entity pairs in the form ROLE:entity."
 # type        = "list"
 #  default     = []
+#}
+
+
+#terraform {
+#  backend "gcs" {
+#    bucket  = "fuchicorp_bucket"
+#    prefix    = "test/terraform.tfstate"    #A path to the data you want to upload
+#   project = "focus-surfer-237100"
+#  }
 #}
